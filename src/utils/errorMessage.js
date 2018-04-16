37d48258a0679b8b52f668bcae2e1c8babbfeb60
Sebastian Kim <sydneyitguy@gmail.com>
2018-04-16 12:07:26 +0900

    Fix error handling

--- src/utils/errorMessage.js
+++ src/utils/errorMessage.js
@@ -1,7 +1,7 @@
 export const extractErrorMessage = function(e) {
   if (e.error_description) {
     const match = e.error_description.match(/.+[A-Z_]+:(.+)/);
-    if (match.length > 1) {
+    if (match && match.length > 1) {
       return match[1];
     } else {
       return e.error_description;
