d06fca203eed167103d29987d09b3d7ac351fb76
Sebastian Kim <sydneyitguy@gmail.com>
2018-04-25 19:00:23 +0900

    Update Steemconnect permission settings

--- src/utils/steemConnectAPI.js
+++ src/utils/steemConnectAPI.js
@@ -4,7 +4,7 @@ const api = sc2.Initialize({
   app: 'steemhunt.com',
   callbackURL: process.env.REACT_APP_STEEMCONNECT_REDIRECT_URL,
   accessToken: 'access_token',
-  scope: [ 'login', 'vote', 'comment', 'comment_delete', 'comment_options', 'custom_json' ],
+  scope: [ 'login', 'vote', 'comment', 'delete_comment', 'comment_options', 'custom_json' ],
 });
 
 export default api;
