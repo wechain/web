2db731086bd6ce08dee16ff1dc9527299ecaf481
Sebastian Kim <sydneyitguy@gmail.com>
2018-06-14 00:33:08 +0900

    default search option for hunt_score

--- src/utils/sortOptions.js
+++ src/utils/sortOptions.js
@@ -1,5 +1,6 @@
 export function getSortOption(key) {
-  return localStorage.getItem(key) || 'payout';
+  // return localStorage.getItem(key) || 'hunt_score';
+  return 'hunt_score';
 }
 export function setSortOption(key, value) {
   return localStorage.setItem(key, value);
