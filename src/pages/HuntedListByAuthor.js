4fb9c7a2d237844be3ce6e585e0efb8f56b8d70f
Sebastian Kim <sydneyitguy@gmail.com>
2018-06-14 01:16:08 +0900

    Make other sort options unavailable

--- src/pages/HuntedListByAuthor.js
+++ src/pages/HuntedListByAuthor.js
@@ -86,10 +86,10 @@ class HuntedListByAuthor extends Component {
               <span className="text-small">Sort by: </span>
               <Select size="small" defaultValue={getSortOption('profile')} onChange={this.handleSortOption}>
                 <Select.Option value="hunt_score">Hunt Score</Select.Option>
-                <Select.Option value="payout">Payout</Select.Option>
-                <Select.Option value="created">New</Select.Option>
-                <Select.Option value="vote_count">Vote Count</Select.Option>
-                <Select.Option value="comment_count">Comment Count</Select.Option>
+                <Select.Option value="payout" disabled>Payout</Select.Option>
+                <Select.Option value="created" disabled>New</Select.Option>
+                <Select.Option value="vote_count" disabled>Vote Count</Select.Option>
+                <Select.Option value="comment_count" disabled>Comment Count</Select.Option>
               </Select>
             </div>
           </div>
