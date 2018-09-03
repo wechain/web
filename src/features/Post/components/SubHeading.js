423e8cfcc91d8d52dc20712b91b44af5ccd2a94b
Sebastian Kim <sydneyitguy@gmail.com>
2018-09-03 23:10:13 +0900

    Fix single quote

--- src/features/Post/components/SubHeading.js
+++ src/features/Post/components/SubHeading.js
@@ -29,7 +29,7 @@ export class SubHeading extends PureComponent {
     const timeLeft = timeUntilMidnightSeoul(true)
 
     if (timeLeft === '00:00:00') {
-      this.setState({ timer: (<div>Today's ranking competition is finished. Please <a onClick={() => window.location.reload()}>refresh your page.</a></div>) });
+      this.setState({ timer: (<div>Today&apos;s ranking competition is finished. Please <a onClick={() => window.location.reload()}>refresh your page.</a></div>) });
       clearInterval(this.interval);
     } else {
       this.setState({ timer: (<div><b>{timeLeft}</b> left till midnight (KST)</div>) });
