02c5d09bb45b8a8c775397a5687e8bce9b74c455
Astro Lee <leonardo.lee1993@gmail.com>
2018-10-02 18:25:18 +0900

    phone number country code

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -4,10 +4,14 @@ import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import isEmpty from 'lodash/isEmpty';
-import { Form, Input, Icon, Button } from 'antd';
+import { Form, Input, Icon, Button, Select } from 'antd';
 import api from 'utils/api';
 import { getLoginURL } from 'utils/token';
 import userImage from 'assets/images/icon-create-account@3x.png';
+import smsImage from 'assets/images/img-phone@3x.png';
+import ReactPhoneInput from 'react-phone-input-2';
+import { isValidNumber } from 'libphonenumber-js'
+import steem from 'steem';
 
 const FormItem = Form.Item;
 
@@ -18,9 +22,12 @@ class SignUp extends Component {
 
   state = {
     pageTitle: 'Create Account',
-    stage: 0,
+    stage: 1,
     accountCheck: null,
     accountCheckMsg: 'Please input your username',
+    accountName: null,
+    phoneCheck: false,
+    phoneNumber: '',
   };
 
   componentDidMount() {
@@ -32,33 +39,53 @@ class SignUp extends Component {
     }
   }
 
-  checkAccount  = (_, value, callback) => {
-    this.setState({ accountCheck: 'loading', accountCheckMsg: 'Checking the server ...' });
-
+  checkAccount = (_, value, callback) => {
     if (!value || value.length === 0) {
       this.setState({ accountCheck: null, accountCheckMsg: 'Please input your username' });
       return callback();
     }
 
-    api.get('/posts.json', { url: value }, true).then((res) => {
-      if (res.result === 'OK') {
-        callback();
-      } else {
-        if (value.length < 4) {
-          this.setState({ accountCheck: false, accountCheckMsg: 'Account name should be longer' });
-          callback('');
-        } else if (value === 'steemhunt') {
-          this.setState({ accountCheck: false, accountCheckMsg: 'This username is already in use' });
-          callback();
-        } else if (value === 'astrocket') {
-          this.setState({ accountCheck: 'validated', accountCheckMsg: `The username ${value} is available` });
-          callback();
-        }
+    if (!/^[a-zA-Z0-9\.\-]+$/.test(value)) {
+      this.setState({ accountCheck: false, accountCheckMsg: 'Account name should contain only letters, digits, periods, and dashes' });
+      return callback();
+    }
+
+    if (value.length < 4) {
+      this.setState({ accountCheck: false, accountCheckMsg: 'Account name should be longer' });
+      return callback();
+    }
+
+    this.setState({ accountName: null, accountCheck: 'loading', accountCheckMsg: 'Checking the server ...' }, () => {
+      try {
+        steem.api.lookupAccountNames([value], (err, result) => {
+          if (result[0] !== null) {
+            this.setState({ accountCheck: false, accountCheckMsg: 'This username is already in use' }, () => { return callback(); });
+          } else {
+            this.setState({ accountName: value, accountCheck: 'validated', accountCheckMsg: <div>The username <span style={{ fontWeight: 'bold' }}>{value}</span> is available</div> }, () => { return callback(); });
+          }
+        });
+      } catch (error) {
+        return callback('Service is temporarily unavailable, Please try again later.');
       }
-    }).catch(msg => {
-      callback('Service is temporarily unavailable, Please try again later.');
     });
   };
+
+  setPhoneNumber = (number) => {
+    this.setState({ phoneNumber: number, phoneCheck: isValidNumber(number) })
+  }
+
+  sendMessage = (e) => {
+    e.preventDefault();
+    const res = api.post('/phone_number/send_sms.json', {phone_number: this.state.phoneNumber});
+    console.log(res);
+  }
+
+  submitAccount = (e) => {
+    e.preventDefault();
+    if (this.state.accountCheck && this.state.accountName !== null) {
+      this.nextStage();
+    }
+  }
 
   validateStatus = (status) => {
     if (status === null) {
@@ -70,21 +97,27 @@ class SignUp extends Component {
     return status === 'validated' ? "success" : "error"
   }
 
+  nextStage = () => {
+    this.setState({
+      stage: this.state.stage += 1,
+    })
+  }
+
   renderForm(stage) {
     const { getFieldDecorator } = this.props.form;
 
     let form;
 
-    switch(stage) {
+    switch (stage) {
       case 0:
         form = (
-          <div className="form-container">
+          <div key={0} className="form-container">
             <img src={userImage} alt="Steem User" />
             <p>
               Choose your username.
               This will be your name that are called in Steemhunt and other Steem-based  apps.
             </p>
-            <Form>
+            <Form onSubmit={this.submitAccount}>
               <FormItem
                 validateStatus={this.validateStatus(this.state.accountCheck)}
                 help={this.state.accountCheckMsg}
@@ -102,7 +135,7 @@ class SignUp extends Component {
               </FormItem>
             </Form>
             <p className="form-tail">
-              Do you already have an account?<br/>
+              Do you already have an account?<br />
               <a href={getLoginURL()} className="action less-margin">
                 Sign In
               </a>
@@ -112,7 +145,30 @@ class SignUp extends Component {
         break;
       case 1:
         form = (
-          <div>fdjisalfjsdal</div>
+          <div key={1} className="form-container">
+            <img src={smsImage} alt="SMS Send" />
+            <p>
+              Enter your phone number.
+              We will send you a text message with a verification code that you’ll need to enter on the next creen.
+            </p>
+            <Form onSubmit={this.sendMessage}>
+              <FormItem
+                style={{ marginBottom: 20 }}
+              >
+                <ReactPhoneInput inputStyle={{height: 40, width: '100%'}} defaultCountry={'us'} value={this.state.phoneNumber} onChange={this.setPhoneNumber}/>
+                {this.state.phoneNumber}
+              </FormItem>
+              <FormItem>
+                <Button type="primary" htmlType="submit" disabled={!this.state.phoneCheck} block>Send SMS</Button>
+              </FormItem>
+            </Form>
+            <p className="form-tail">
+              Do you already have an account?<br />
+              <a href={getLoginURL()} className="action less-margin">
+                Sign In
+              </a>
+            </p>
+          </div>
         )
         break;
       default:
